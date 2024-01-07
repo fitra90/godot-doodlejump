a8a3f01f331f814bfd31ad92f37aec0007edb254
Fitra Fadilana <fitra90@gmail.com>
2024-01-07 17:49:35 +0700

    init commit

---
+++ scripts/player.gd
@@ -0,0 +1,43 @@
+extends CharacterBody2D
+class_name Player
+
+@onready var animator =  $AnimationPlayer
+
+var speed = 300.0
+var gravity = 15.0
+var max_fall_velocity = 1000.0
+var viewport_size
+var jump_velocity = -800.0
+
+func _ready():
+	viewport_size = get_viewport_rect().size
+
+func _process(delta):
+	if velocity.y > 0:
+		if animator.current_animation != "Fall":
+			animator.play("Fall")
+	elif velocity.y < 0:
+		if animator.current_animation != "Jump":
+			animator.play("Jump")
+
+func _physics_process(delta):
+	velocity.y += gravity
+	if velocity.y > max_fall_velocity:
+		velocity.y = max_fall_velocity
+	
+	var direction = Input.get_axis("move_left", "move_right")
+	if direction:
+		velocity.x = speed * direction
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+		
+	move_and_slide()
+	
+	var margin = 20
+	if global_position.x > viewport_size.x + margin:
+		global_position.x = -margin
+	if global_position.x < -margin:
+		global_position.x = viewport_size.x + margin
+
+func jump():
+	velocity.y = jump_velocity
